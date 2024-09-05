9d8083986d8c9d5841b2bfaf69627e2d0bcf5749
Mubin Codemen <codemen.almubin@gmail.com>
2024-09-05 22:58:41 +0600

    get and get by id, update delete completed.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import models from '../models/index.js';
-import { saveUser, getAllUsers } from '../services/userService.js';
+import { saveUser, getAllUsers, update, deleteById, getById } from '../services/userService.js';
 const router = express.Router();
 
 const postHandler = async (req, res) => {
     const body = req.body;
     const user = await saveUser(body);
-    res.send(user._id);
+    res.status(201).send(user._id);
 
 }
 
@@ -14,12 +14,49 @@ const getHaadler = async (req, res) => {
     // const q = JSON.stringify(req.query.dfsdf);
     const users = await getAllUsers();
 
-    res.send(users)
+    res.status(200).send(users)
+}
+
+const deleteHandler = async (req, res) => {
+    const id = req.query.id;
+
+    // console.log('req');
+    // console.log(req)
+
+    if (!id) {
+        res.status(400).send("Id not provided");
+    }
+
+    await deleteById(id);
+    res.status(200).send("User deleted.")
+}
+
+const putHandler = async (req, res) => {
+    const body = req.body;
+
+    const user = await update(body);
+    res.status(200).send(user._id);
+}
+
+const getByQueryHaadler = async (req, res) => {
+    const id = req.params.id;
+
+    // console.log('req');
+    // console.log(req)
+
+    if (!id) {
+        res.status(400).send("Id not provided");
+    }
+
+    const user = await getById(id);
+    res.status(200).send(user)
 }
 
 router.get('/', getHaadler)
-
+router.get('/:id', getByQueryHaadler)
 router.post('/', postHandler)
+router.put('/', putHandler);
+router.delete('/', deleteHandler)
 
 const configure = (app) => {
     app.use('/users', router)
--- services/userService.js
+++ services/userService.js
@@ -12,3 +12,38 @@ export const getAllUsers = async () => {
 
     return users;
 }
+
+export const update = async (user) => {
+    const id = user._id;
+    const User = models.User;
+
+    let model = await User.findById(id);
+
+    if (model) {
+        model.username = user.username;
+        model.save()
+        return model;
+    }
+
+    return null;
+}
+
+export const getById = async (id) => {
+    const User = models.User;
+
+    let model = await User.findById(id);
+
+    if (model) {
+        return model;
+    }
+
+    return null;
+}
+
+export const deleteById = async (id) => {
+    const User = models.User;
+
+    const result = await User.deleteOne({ _id: id });
+
+    return result;
+}
